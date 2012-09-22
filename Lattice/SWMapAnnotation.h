af6e9f9f036fceb240341f17af8ea9714305e1ce
Kent McCullough <kent@kentmccullough.com>
2012-09-21 23:03:09 -0500

    added support for geolocation annotations

---
+++ Lattice/SWMapAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  SWMapAnnotation.h
+//  Lattice
+//
+//  Created by Kent McCullough on 9/21/12.
+//  Copyright (c) 2012 Sam Warmuth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SWMapAnnotation : NSObject
+
+@end
---
+++ Lattice/SWMapAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  SWMapAnnotation.m
+//  Lattice
+//
+//  Created by Kent McCullough on 9/21/12.
+//  Copyright (c) 2012 Sam Warmuth. All rights reserved.
+//
+
+#import "SWMapAnnotation.h"
+
+@implementation SWMapAnnotation
+
+@end
